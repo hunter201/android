db64ef27f32a3c005d2a743d34163654e5156087
Rad <ggggg@MacBook-Pro-ggggg.local>
2021-12-01 19:27:18 +0400

    Created SecondActivity

--- app/src/main/java/com/example/myapplication/UserAdapter.kt
+++ app/src/main/java/com/example/myapplication/UserAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class UserAdapter(private var userList: List<User>) : RecyclerView.Adapter<UserViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        TODO("Not yet implemented")
+
         val itemView = LayoutInflater.from(parent.context).inflate(
             R.layout.user_list_item,
             parent, false
@@ -15,13 +15,13 @@ class UserAdapter(private var userList: List<User>) : RecyclerView.Adapter<UserV
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
         val user = userList[position]
         holder.bind(user)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
         return userList.size
     }
 }
