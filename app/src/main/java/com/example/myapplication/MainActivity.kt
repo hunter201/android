701e32e3052d584e0c2a6748dddf2d53db8945bf
Rad <ggggg@MacBook-Pro-ggggg.local>
2021-12-04 19:19:34 +0400

    Added fragments realization

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,19 +2,47 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-lateinit var userRecyclerView: RecyclerView
+//lateinit var userRecyclerView: RecyclerView
+lateinit var bottomNavigationMenu: BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    fragment = FirstFragment()
+                }
+                R.id.page_2 -> {
+                    fragment = SecondFragment()
+                }
+                R.id.page_3 -> {
+                    fragment = ThirdFragment()
+                }
+            }
+            replaceFragment(fragment!!)
+            true
+        }
+        bottomNavigationMenu.selectedItemId = R.id.page_1
+    }
 
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 //        val userList: List<User> = listOf(
 //            User("Ivan", 20),
 //            User("Helen", 19),
@@ -29,9 +57,6 @@ class MainActivity : AppCompatActivity() {
 //            false
 //        )
 //        userRecyclerView.adapter = UserAdapter(userList)
-    }
-
-
-    //RecyclerView
-
 }
+
+
